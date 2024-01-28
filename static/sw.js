ef2a009e9a3f7f4d9d161a84c5af3594e5fdad5a
reblack Network <151373499+reblacknetwork@users.noreply.github.com>
2024-01-27 19:09:19 -0500

    Delete static directory

--- static/sw.js
+++
@@ -1,8 +0,0 @@
-/*global UVServiceWorker,__uv$config*/
-importScripts('/m/bundle.js');
-importScripts('/m/config.js');
-importScripts(__uv$config.sw || '/m/sw.js');
-
-const sw = new UVServiceWorker();
-
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
